dc2f9ddf3cc7859d02c2c1a0b00595d1d7509f3a
immrakpk <devops@talcya.com>
2025-11-03 09:17:43 -0500

    chore(lint): add backend ESLint overrides to ease migration to TS 5.9

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,6 +1,26 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    // Relax strict rules that may surface after parser upgrades until we adjust codebase
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    // Allow some migration noise temporarily
+    '@typescript-eslint/no-empty-function': 'warn',
+  },
+};
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     // avoid enabling type-aware rules that require a TS project to prevent issues with mixed .js files
     sourceType: 'module',
